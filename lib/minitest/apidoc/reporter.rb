d88f656968ceb1bc088e7a784aba1dfaa328f2fd
shadowbq <shadowbq@gmail.com>
2015-12-09 01:25:51 -0500

    fixing coderay force json on non json requests

--- lib/minitest/apidoc/reporter.rb
+++ lib/minitest/apidoc/reporter.rb
@@ -24,12 +24,24 @@ module Minitest
               if (test.last_request.body.string == "") || (test.last_request.body.string == nil)
                 request_body = false
               else
-                request_body = CodeRay.scan(JSON.pretty_generate(JSON.parse(test.last_request.body.string)), :JSON).div
+                #require 'pry'
+                #binding.pry
+                if test.last_request.content_type == "application/json"
+                  begin
+                    request_body = CodeRay.scan(JSON.pretty_generate(JSON.parse(test.last_request.body.string)), :JSON).div
+                  rescue
+                    request_body = false
+                  end
+                elsif test.last_request.content_type.includes? "CONTENT_TYPE"=>"multipart/form-data;"
+                  request_body false
+                else
+                  request_body = test.last_request.body.string
+                end
               end
             rescue NoMethodError
               request_body = false
             end
-            
+
             # Check if test had a pre-defined status code
             if !(test.class.status_codes.first.nil?) && test.class.status_codes.first.has_key?(:code)
                 @endpoints[test.class].status_codes = test.class.status_codes
@@ -42,12 +54,22 @@ module Minitest
             end
           end
 
+          begin
+            if test.last_response.content_type == "application/json"
+              formatted_response = CodeRay.scan(test.class.metadata[:response], :JSON).div
+            else
+              formatted_response = test.class.metadata[:response]
+            end
+          rescue
+            formatted_response = test.class.metadata[:response]
+          end
+
           if test.passed? && test.class.metadata[:request]
             @endpoints[test.class].examples << {
               :title    => test.class.metadata[:example_name],
               :request  => test.class.metadata[:request],
               :request_body  => request_body,
-              :response => CodeRay.scan(test.class.metadata[:response], :JSON).div
+              :response => formatted_response
             }
           end
         rescue Rack::Test::Error => e
--- lib/minitest/apidoc/version.rb
+++ lib/minitest/apidoc/version.rb
@@ -1,5 +1,5 @@
 module Minitest
   module Apidoc
-    VERSION = "0.3.5"
+    VERSION = "0.3.6"
   end
 end
